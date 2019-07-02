84dbc6b94abcb1e34a4d344774d8cc0d919a9880
michaeldavie <michael.davie@gmail.com>
2019-07-01 23:21:22 -0400

    Return labels in chosen language

--- env_canada/ec_data.py
+++ env_canada/ec_data.py
@@ -9,54 +9,171 @@ import requests
 class ECData(object):
     SITE_LIST_URL = 'http://dd.weatheroffice.ec.gc.ca/citypage_weather/docs/site_list_en.csv'
     XML_URL_BASE = 'http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/{}_{}.xml'
-    value_paths = {
-        'temperature': './currentConditions/temperature',
-        'dewpoint': './currentConditions/dewpoint',
-        'wind_chill': './currentConditions/windChill',
-        'humidex': './currentConditions/humidex',
-        'pressure': './currentConditions/pressure',
-        'humidity': './currentConditions/relativeHumidity',
-        'visibility': './currentConditions/visibility',
-        'condition': './currentConditions/condition',
-        'wind_speed': './currentConditions/wind/speed',
-        'wind_gust': './currentConditions/wind/gust',
-        'wind_dir': './currentConditions/wind/direction',
-        'wind_bearing': './currentConditions/wind/bearing',
-        'text_summary': './forecastGroup/forecast/textSummary',
-        'high_temp': './forecastGroup/forecast/temperatures/temperature[@class="high"]',
-        'low_temp': './forecastGroup/forecast/temperatures/temperature[@class="low"]',
-        'pop': './forecastGroup/forecast/abbreviatedForecast/pop',
-        'timestamp': './currentConditions/dateTime/timeStamp',
-        'location': './location/name',
-        'station': './currentConditions/station',
-        'icon_code': './currentConditions/iconCode'
-    }
-
-    attribute_paths = {
-        'forecast_period': {
-            'xpath': './forecastGroup/forecast/period',
-            'attribute': 'textForecastName'
+    conditions_meta = {
+        'temperature': {
+            'xpath': './currentConditions/temperature',
+            'english': 'Temperature',
+            'french': 'Température'
+        },
+        'dewpoint': {
+            'xpath': './currentConditions/dewpoint',
+            'english': 'Dew Point',
+            'french': 'Point de rosée'
+        },
+        'wind_chill': {
+            'xpath': './currentConditions/windChill',
+            'english': 'Wind Chill',
+            'french': 'Refroidissement éolien'
+        },
+        'humidex': {
+            'xpath': './currentConditions/humidex',
+            'english': 'Humidex',
+            'french': 'Humidex'
+        },
+        'pressure': {
+            'xpath': './currentConditions/pressure',
+            'english': 'Pressure',
+            'french': 'Pression'
         },
         'tendency': {
             'xpath': './currentConditions/pressure',
-            'attribute': 'tendency'
+            'attribute': 'tendency',
+            'english': 'Tendency',
+            'french': 'Tendance'
+        },
+        'humidity': {
+            'xpath': './currentConditions/relativeHumidity',
+            'english': 'Humidity',
+            'french': 'Humidité'
+        },
+        'visibility': {
+            'xpath': './currentConditions/visibility',
+            'english': 'Visibility',
+            'french': 'Visibilité'
+        },
+        'condition': {
+            'xpath': './currentConditions/condition',
+            'english': 'Condition',
+            'french': 'Condition'
+        },
+        'wind_speed': {
+            'xpath': './currentConditions/wind/speed',
+            'english': 'Wind Speed',
+            'french': 'Vitesse de vent'
+        },
+        'wind_gust': {
+            'xpath': './currentConditions/wind/gust',
+            'english': 'Wind Gust',
+            'french': 'Rafale de vent'
+        },
+        'wind_dir': {
+            'xpath': './currentConditions/wind/direction',
+            'english': 'Wind Direction',
+            'french': 'Direction de vent'
+        },
+        'wind_bearing': {
+            'xpath': './currentConditions/wind/bearing',
+            'english': 'Wind Bearing',
+            'french': 'Palier de vent'
+        },
+        'forecast_period': {
+            'xpath': './forecastGroup/forecast/period',
+            'attribute': 'textForecastName',
+            'english': 'Forecast Period',
+            'french': 'Période de prévision'
+        },
+        'text_summary': {
+            'xpath': './forecastGroup/forecast/textSummary',
+            'english': 'Text Summary',
+            'french': 'Résumé textuel'
+        },
+        'high_temp': {
+            'xpath': './forecastGroup/forecast/temperatures/temperature[@class="high"]',
+            'english': 'High Temperature',
+            'french': 'Haute température'
+        },
+        'low_temp': {
+            'xpath': './forecastGroup/forecast/temperatures/temperature[@class="low"]',
+            'english': 'Low Temperature',
+            'french': 'Basse température'
+        },
+        'pop': {
+            'xpath': './forecastGroup/forecast/abbreviatedForecast/pop',
+            'english': 'Chance of Precip.',
+            'french': 'Probabilité d\'averses'
+        },
+        'timestamp': {
+            'xpath': './currentConditions/dateTime/timeStamp',
+            'english': 'Timestamp',
+            'french': 'Horodatage'
+        },
+        'location': {
+            'xpath': './location/name',
+            'english': 'Location',
+            'french': 'Location'
+        },
+        'station': {
+            'xpath': './currentConditions/station',
+            'english': 'Station',
+            'french': 'Station'
+        },
+        'icon_code': {
+            'xpath': './currentConditions/iconCode',
+            'english': 'Icon Code',
+            'french': 'Code icône'
         }
     }
 
-    alert_patterns = {
-        'english': {
-            'warnings': '.*WARNING((?!ENDED).)*$',
-            'watches': '.*WATCH((?!ENDED).)*$',
-            'advisories': '.*ADVISORY((?!ENDED).)*$',
-            'statements': '.*STATEMENT((?!ENDED).)*$',
-            'endings': '.*ENDED'
+    alerts_meta = {
+        'warnings': {
+            'english': {
+                'label': 'Warnings',
+                'pattern': '.*WARNING((?!ENDED).)*$'
+            },
+            'french': {
+                'label': 'Alertes',
+                'pattern': '.*ALERTE((?!TERMINÉ).)*$'
+            }
         },
-        'french': {
-            'warnings': '.*ALERTE((?!TERMINÉ).)*$',
-            'watches': '.*VEILLE((?!TERMINÉ).)*$',
-            'advisories': '.*AVIS((?!TERMINÉ).)*$',
-            'statements': '.*BULLETIN((?!TERMINÉ).)*$',
-            'endings': '.*TERMINÉE?'
+        'watches': {
+            'english': {
+                'label': 'Watches',
+                'pattern': '.*WATCH((?!ENDED).)*$'
+            },
+            'french': {
+                'label': 'Veilles',
+                'pattern': '.*VEILLE((?!TERMINÉ).)*$'
+            }
+        },
+        'advisories': {
+            'english': {
+                'label': 'Advisories',
+                'pattern': '.*ADVISORY((?!ENDED).)*$'
+            },
+            'french': {
+                'label': 'Avis',
+                'pattern': '.*AVIS((?!TERMINÉ).)*$'
+            }
+        },
+        'statements': {
+            'english': {
+                'label': 'Statements',
+                'pattern': '.*STATEMENT((?!ENDED).)*$'
+            },
+            'french': {
+                'label': 'Bulletins',
+                'pattern': '.*BULLETIN((?!TERMINÉ).)*$'
+            }
+        },
+        'endings': {
+            'english': {
+                'label': 'Endings',
+                'pattern': '.*ENDED'
+            },
+            'french': {
+                'label': 'Terminaisons',
+                'pattern': '.*TERMINÉE?'
+            }
         }
     }
 
@@ -89,17 +206,22 @@ class ECData(object):
         # Update current conditions
         self.conditions = {}
 
-        for condition, xpath in self.value_paths.items():
-            result = xml_object.findtext(xpath)
-            if result:
-                self.conditions[condition] = result
+        for c, meta in self.conditions_meta.items():
+            self.conditions[c] = {'label': meta[self.language]}
+            value = None
 
-        for condition, v in self.attribute_paths.items():
-            element = xml_object.find(v['xpath'])
+            element = xml_object.find(meta['xpath'])
+
             if element is not None:
-                value = element.attrib.get(v['attribute'])
-                if value:
-                    self.conditions[condition] = value
+                if meta.get('attribute'):
+                    value = element.attrib.get(meta['attribute'])
+                else:
+                    value = element.text
+
+                if element.attrib.get('units'):
+                    self.conditions[c].update({'unit': element.attrib.get('units')})
+
+            self.conditions[c].update({'value': value})
 
         # Update alerts
         alert_elements = xml_object.findall('./warnings/event')
@@ -113,10 +235,13 @@ class ECData(object):
             date_pattern = 'p:contains("{}") span'
             detail_pattern = 'p:contains("{}") ~ p'
 
-            for category, pattern in self.alert_patterns[self.language].items():
-                self.alerts[category] = []
+            for category, meta in self.alerts_meta.items():
+                meta = meta[self.language]
+                self.alerts[category] = {'value': [],
+                                         'label': meta['label']}
+
                 for a in alert_list:
-                    title_match = re.search(pattern, a)
+                    title_match = re.search(meta['pattern'], a)
                     if title_match:
                         alert = {'title': a,
                                  'date': '',
@@ -135,7 +260,7 @@ class ECData(object):
                         if detail_match:
                             alert.update({'detail': detail_match[0].text})
 
-                        self.alerts[category].append(alert)
+                        self.alerts[category]['value'].append(alert)
 
         # Update daily forecasts
         self.forecast_time = xml_object.findtext('./forecastGroup/dateTime/timeStamp')
