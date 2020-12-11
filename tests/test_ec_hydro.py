1d2da7a51a9617c671994b34465f0468936a7dd6
michaeldavie <michael.davie@gmail.com>
2020-12-10 22:54:47 -0500

    Improve hydro tests

--- tests/test_ec_hydro.py
+++ tests/test_ec_hydro.py
@@ -18,13 +18,20 @@ def test_get_hydro_sites():
 def test_echydro(init_parameters):
     hydro = ECHydro(**init_parameters)
     assert isinstance(hydro, ECHydro)
+    asyncio.run(hydro.update())
+    assert isinstance(hydro.timestamp, datetime)
+    assert isinstance(hydro.measurements["water_level"]["value"], float)
+    if hydro.measurements.get("discharge"):
+        assert isinstance(hydro.measurements["discharge"]["value"], float)
 
 
 @pytest.fixture()
 def test_hydro():
-    return ECHydro(coordinates=(50, -100))
+    return ECHydro(province="ON", station="02KF005")
 
 
 def test_update(test_hydro):
     asyncio.run(test_hydro.update())
-    assert test_hydro.measurements["water_level"]
+    assert isinstance(test_hydro.timestamp, datetime)
+    assert isinstance(test_hydro.measurements["water_level"]["value"], float)
+    assert isinstance(test_hydro.measurements["discharge"]["value"], float)
