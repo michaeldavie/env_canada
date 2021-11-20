c62d80605fe53c8c7555db018ed5afc336d87842
Glenn Waters <gwwaters@gmail.com>
2021-11-19 23:23:28 -0500

    Cleanup and docs.

--- env_canada/ec_cache.py
+++ env_canada/ec_cache.py
@@ -5,12 +5,12 @@ CACHE_EXPIRE_TIME = timedelta(minutes=120)
 
 
 class CacheClientSession(ClientSession):
+    """Shim to cache ClientSession requests."""
+
     _cache = {}
 
-    def __init__(self, raise_for_status=False):
-        super().__init__(raise_for_status=raise_for_status)
-
     def _flush_cache(self):
+        """Flush expired cache entries."""
         expiry_time = datetime.now()
         to_delete = [k for k,v in self._cache.items() if v[0] < expiry_time]
 
@@ -19,13 +19,15 @@ class CacheClientSession(ClientSession):
             del self._cache[k]
 
     async def get(self, url, params, cache_time=CACHE_EXPIRE_TIME):
+        """Thin wrapper around ClientSession.get to cache responses."""
+        self._flush_cache()
+
         cache_key = (url, tuple(sorted(params.items())))
         result = self._cache.get(cache_key)
 
         if not result:
-            print(f"cached get {'' if result else 'NOT '}found!   {cache_key}")
             result = (datetime.now() + cache_time, await super().get(url=url, params=params))
             self._cache[cache_key] = result
+            print(f"cached get NOT found!  {cache_key}")
 
-        self._flush_cache()
         return result[1]
