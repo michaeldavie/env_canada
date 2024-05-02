3cfe88e2cea48fffd4db442f75c218c595b46382
Michael Davie <michael.davie@gmail.com>
2024-05-02 16:29:09 -0400

    Remove test with no conditions

--- tests/test_ec_weather.py
+++ tests/test_ec_weather.py
@@ -17,19 +17,11 @@ def test_ecweather(init_parameters):
     weather = ECWeather(**init_parameters)
     assert isinstance(weather, ECWeather)
 
-@pytest.fixture()
-def no_conditions():
-    return ECWeather(station_id="ON/s0000773")
-
-def test_update_no_conditions(no_conditions):
-    asyncio.run(no_conditions.update())
-    assert no_conditions.conditions == {}
-    assert no_conditions.daily_forecasts
-    assert no_conditions.hourly_forecasts
 
 @pytest.fixture()
 def with_conditions():
     return ECWeather(station_id="ON/s0000430")
+
 
 def test_update_with_conditions(with_conditions):
     asyncio.run(with_conditions.update())
