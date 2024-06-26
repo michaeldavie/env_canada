dab6bf09532e68166744872cfc5d4dfd87c94b36
Glenn Waters <glenn@watrs.ca>
2024-06-26 06:51:17 -0400

    Tidy Cache

--- env_canada/ec_cache.py
+++ env_canada/ec_cache.py
@@ -7,17 +7,25 @@ class Cache:
     _cache = {}
 
     @classmethod
+    def flush(cls):
+        """Empty the cache."""
+        _cache = {}  # type: ignore
+
+    @classmethod
     def add(cls, cache_key, item, cache_time=CACHE_EXPIRE_TIME):
+        """Add an entry to the cache."""
+
         cls._cache[cache_key] = (datetime.now() + cache_time, item)
         return item  # Returning item useful for chaining calls
 
     @classmethod
     def get(cls, cache_key):
-        # Flush at start so we don't use expired entries
+        """Get an entry from the cache."""
+
+        # Delete expired entries at start so we don't use expired entries
         now = datetime.now()
         expired = [key for key, value in cls._cache.items() if value[0] < now]
         for key in expired:
-            # print(f"Flushing: {key}")
             del cls._cache[key]
 
         result = cls._cache.get(cache_key)
