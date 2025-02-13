e213846b1e689a6519ad8ae43228a9fa6205ec6e
Glenn Waters <glenn@watrs.ca>
2025-02-13 13:01:08 -0500

    Add local radar test.

--- tests/ec_radar_test.py
+++ tests/ec_radar_test.py
@@ -1,13 +1,16 @@
 import asyncio
 from datetime import date, datetime
 from io import BytesIO
+from unittest.mock import AsyncMock, patch
 
 import pytest
 from PIL import Image
+from syrupy.assertion import SnapshotAssertion
 
 from env_canada import ECRadar
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize(
     "init_parameters",
     [
@@ -29,17 +32,20 @@ def test_radar():
     return ECRadar(coordinates=(50, -100))
 
 
+@pytest.mark.slow
 def test_get_dimensions(test_radar):
     dimensions = asyncio.run(test_radar._get_dimensions())
     assert isinstance(dimensions[0], datetime) and isinstance(dimensions[1], datetime)
 
 
+@pytest.mark.slow
 def test_get_latest_frame(test_radar):
     frame = asyncio.run(test_radar.get_latest_frame())
     image = Image.open(BytesIO(frame))
     assert image.format == "PNG"
 
 
+@pytest.mark.slow
 def test_get_loop(test_radar):
     loop = asyncio.run(test_radar.get_loop())
     image = Image.open(BytesIO(loop))
@@ -54,3 +60,24 @@ def test_set_precip_type(test_radar):
         assert test_radar.precip_type[1] == "rain"
     else:
         assert test_radar.precip_type[1] == "snow"
+
+
+@pytest.mark.asyncio
+async def test_get_radar_image_with_mock_data(test_radar, snapshot: SnapshotAssertion):
+    """
+    This is still technically a slow test, in that it uses a lot of CPU.
+    Still useful as it is a complete test of the entire radar code
+    except for going to the network.
+    """
+
+    def mock_get_resource(_, params, bytes=True):
+        fname = f"tests/fixtures/radar/{params['request']}_{params.get('time', '')}"
+        with open(fname, "rb") as f:
+            return f.read()
+
+    with patch(
+        "env_canada.ec_radar._get_resource", AsyncMock(side_effect=mock_get_resource)
+    ):
+        await test_radar.update()
+
+    assert test_radar == snapshot
